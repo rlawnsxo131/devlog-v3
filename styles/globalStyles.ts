7afdf4996ad876feb1365832dad95f44eddc37e8
juntae <rlawnsxo131@gmail.com>
2022-09-07 00:53:57 +0900

    refactor: global style 선언 조금 수정

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -1,33 +1,37 @@
 import { globalCss } from './_stitches.config';
 
-const topLevelBoxStyle = {
+const topLevelBoxStyles = {
   margin: '0',
   padding: '0',
   height: '100%',
 };
 
+const topLevelBoxChildrenStyles = {
+  boxSizing: 'inherit',
+  fontFamily:
+    '"Montserrat", sans-serif, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", "Noto Sans CJK KR", arial, 돋움, Dotum, Tahoma, Geneva, sans-serif',
+  textRendering: 'optimizeLegibility',
+  color: '$gray12',
+  '&::selection': {
+    color: '$whiteA12',
+    background: '$cyan9',
+  },
+};
+
 const globalStyles = globalCss({
   html: {
-    ...topLevelBoxStyle,
+    ...topLevelBoxStyles,
     boxSizing: 'border-box',
-    '& *': {
-      boxSizing: 'inherit',
-      fontFamily:
-        '"Montserrat", sans-serif, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", "Noto Sans CJK KR", arial, 돋움, Dotum, Tahoma, Geneva, sans-serif',
-      textRendering: 'optimizeLegibility',
-      color: '$gray12',
-      '&::selection': {
-        color: '$whiteA12',
-        background: '$cyan9',
-      },
-    },
     background: '$bg',
+    '& *': {
+      ...topLevelBoxChildrenStyles,
+    },
   },
   body: {
-    ...topLevelBoxStyle,
+    ...topLevelBoxStyles,
   },
   '#__next': {
-    ...topLevelBoxStyle,
+    ...topLevelBoxStyles,
   },
   a: {
     cursor: 'pointer',
