0496e09409b579de73d85b976431556bba748894
juntae <rlawnsxo131@gmail.com>
2022-09-10 02:21:02 +0900

    refactor: useThemeActions 에 zustand shallow 추가

--- hooks/theme/useThemeActions.ts
+++ hooks/theme/useThemeActions.ts
@@ -1,7 +1,14 @@
 import useThemeStore from './_useThemeStore';
+import shallow from 'zustand/shallow';
 
 export default function useThemeActions() {
-  const { setLightTheme, setDarkTheme } = useThemeStore((store) => store);
+  const { setLightTheme, setDarkTheme } = useThemeStore(
+    (store) => ({
+      setLightTheme: store.setLightTheme,
+      setDarkTheme: store.setDarkTheme,
+    }),
+    shallow,
+  );
 
   return {
     setLightTheme,
