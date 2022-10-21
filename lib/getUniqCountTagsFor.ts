751775a277b548cd39a9accf518301028458d848
juntae <rlawnsxo131@gmail.com>
2022-10-22 03:29:33 +0900

    refactor: getUniqCount 함수내 필요없는 코드 제거

--- lib/getUniqCountTagsFor.ts
+++ lib/getUniqCountTagsFor.ts
@@ -1,24 +1,16 @@
 import { CountTag, Post } from '@/types';
 
 export default function getUniqCountTagsFor(posts: Post[]) {
-  const result = new Map<string, number>();
-  const allTags = posts.map((post) => post.tags).flat();
-  for (const tag of allTags) {
-    if (result.has(tag)) {
-      const prevCount = result.get(tag);
-      result.set(tag, prevCount + 1);
-      continue;
-    }
-    result.set(tag, 1);
-  }
-
-  return allTags.reduce<CountTag>((acc, tag) => {
-    if (acc[tag]) {
-      const prevCount = acc[tag];
-      acc[tag] = prevCount + 1;
-    } else {
-      acc[tag] = 1;
-    }
-    return acc;
-  }, {});
+  return posts
+    .map((post) => post.tags)
+    .flat()
+    .reduce<CountTag>((acc, tag) => {
+      if (acc[tag]) {
+        const prevCount = acc[tag];
+        acc[tag] = prevCount + 1;
+      } else {
+        acc[tag] = 1;
+      }
+      return acc;
+    }, {});
 }
