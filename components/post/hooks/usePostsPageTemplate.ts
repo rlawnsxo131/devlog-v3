7ee490540842b5e5a38e036a4cdf927bb6fb23e0
juntae <rlawnsxo131@gmail.com>
2022-11-28 10:40:20 +0900

    refactor: 없어도 될것같은 memoization 제거

--- components/post/hooks/usePostsPageTemplate.ts
+++ components/post/hooks/usePostsPageTemplate.ts
@@ -8,29 +8,28 @@ export default function usePostsPageTemplate() {
   const timerRef = useRef<NodeJS.Timeout | null>(null);
   const { info, error } = useToast();
 
-  const handleCopyToClipboard = useCallback(
-    async (e: React.MouseEvent<HTMLButtonElement>) => {
-      const { title, description, slug } = e.currentTarget.dataset;
-      const url = `${SiteConfig.url}/post/${slug}`;
+  const handleCopyToClipboard = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    const { title, description, slug } = e.currentTarget.dataset;
+    const url = `${SiteConfig.url}/post/${slug}`;
 
-      if (timerRef.current) clearTimeout(timerRef.current);
-      timerRef.current = setTimeout(() => {
-        sharePost(
-          {
-            title,
-            text: description,
-            url,
-          },
-          () => {
-            info('포스트 주소가 복사 되었습니다.');
-          },
-        ).catch((_) => {
-          error('알 수 없는 에러가 발생했습니다.');
-        });
-      }, 250);
-    },
-    [info, error, timerRef],
-  );
+    if (timerRef.current) clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      sharePost(
+        {
+          title,
+          text: description,
+          url,
+        },
+        () => {
+          info('포스트 주소가 복사 되었습니다.');
+        },
+      ).catch((_) => {
+        error('알 수 없는 에러가 발생했습니다.');
+      });
+    }, 250);
+  };
 
   return {
     handleCopyToClipboard,
