fa5536542ee97d13e4965fedd6646f12d32862c6
juntae <rlawnsxo131@gmail.com>
2022-12-14 12:44:50 +0900

    style: post thumbnail css 변경

--- components/post/PostThumbnail.tsx
+++ components/post/PostThumbnail.tsx
@@ -42,17 +42,14 @@ const block = css({
 });
 
 const thumbnailBlock = css({
+  position: 'absolute !important',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
   borderRadius: '0.5rem',
-  '& img': {
-    position: 'absolute !important',
-    top: '0',
-    left: '0',
-    width: '100%',
-    height: '100%',
-    borderRadius: '0.5rem',
-    objectFit: 'cover',
-    zIndex: '$thumbnail',
-  },
+  objectFit: 'cover !important',
+  zIndex: '$thumbnail',
 });
 
 const thumbnailSkeleton = css({
